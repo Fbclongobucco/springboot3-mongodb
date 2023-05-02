1480f34291ccc7f9a55cce8d5b771b8ea63fc6a6
Fbclongobucco <longobuccofbc@gmail.com>
2023-05-02 19:31:13 -0300

    first commit

---
+++ src/test/java/com/longobuccodev/springmongodb/ProjectSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longobuccodev.springmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
